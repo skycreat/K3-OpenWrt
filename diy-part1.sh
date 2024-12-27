8b452a3dccdff9c0ec71cde6f079ae6f283e54af
风 <skycreat@gmail.com>
2024-12-27 21:39:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,11 +7,6 @@
 # See /LICENSE for more information.
 #
 
-echo '添加kenzok8 openwrt-package'
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo '========= kenzok8 添加完成 OK!========='
-
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile
 sed -i '140,412d' target/linux/bcm53xx/image/Makefile
